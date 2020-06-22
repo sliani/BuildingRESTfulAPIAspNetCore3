545e6a5abced21354803a19cbe343c7143d68bff
sliani <liani.sofiane@gmail.com>
2020-06-22 16:57:59 +0200

    First commit

---
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -0,0 +1,40 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase //ControllerBase (Accès aux models, User actuel etc...)
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository)
+        {
+            _courseLibraryRepository = courseLibraryRepository ??
+                throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet()]
+        public IActionResult GetAuthors() // IActionResult renvoi le résultat d'une méthode d'action
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(authorsFromRepo);
+        }
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId) // IActionResult renvoi le résultat d'une méthode d'action
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+
+            if (authorsFromRepo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(authorsFromRepo);
+        }
+    }
+}
